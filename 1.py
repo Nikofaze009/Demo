ec5dd7d10e2feb91d962c3560e4e174bc022ce1f
Pratham <79462080+Nikofaze009@users.noreply.github.com>
2026-01-17 01:05:53 +0530

    update

--- 1.py
+++ 1.py
@@ -2,6 +2,6 @@ import sys
 
 if len(sys.argv) == 2:
     script_name = sys.argv[0]
-    cel = float(sys.argv[1])
+    cel = (sys.argv[1])
     ferh = (cel * 9/5) + 32
     print(f"{cel} Celsius is {ferh} Fahrenheit")
